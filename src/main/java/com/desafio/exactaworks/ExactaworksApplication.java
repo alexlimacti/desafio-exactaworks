d9237ab3e33807fc293d3b36e1460198141fc80a
Alexsandro Lima <alexsandrolima.dev@gmail.com>
2021-12-30 16:29:50 -0300

    create project

---
+++ src/main/java/com/desafio/exactaworks/ExactaworksApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.exactaworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExactaworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExactaworksApplication.class, args);
+    }
+
+}
